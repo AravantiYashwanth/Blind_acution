deb3e37e5a38c8cbdeb2e849db64b247a19a59c8
AravantiYashwanth <yashwantharavanti@gmail.com>
2024-06-21 06:36:15 -0700

    corrected spelling mistakes

--- Blind_auction.py
+++ Blind_auction.py
@@ -2,27 +2,27 @@ from click import clear
 import art
 print(art.logo)
 
-acution = {}
+auction = {}
 bidding_finished = True
 
 
-def blind_acution():
+def blind_auction():
     name = input("What is your name?: ")
     bid = input("What's your bid? ")
-    acution[name] = bid
+    auction[name] = bid
 
 
 print("Welcome to the secret auction program")
-blind_acution()
+blind_auction()
 ans = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
 
 while bidding_finished :
     if ans == "yes" :
         clear()
-        blind_acution()
+        blind_auction()
         ans = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
     if ans == "no" :
-        max_key = max(acution, key=lambda k: acution[k])
-        print(f"The winner is {max_key} with a bid of ${acution[max_key]}")
+        max_key = max(auction, key=lambda k: auction[k])
+        print(f"The winner is {max_key} with a bid of ${auction[max_key]}")
         bidding_finished = False
                 
